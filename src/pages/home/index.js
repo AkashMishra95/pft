f8a4e59dc3cff504b95c0aa6d2da69629d83612b
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-06-08 21:38:40 +0530

    Version 0.2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -46,6 +46,12 @@ export const Home = () => {
                       <div className="ring two"></div>
                       <div className="ring three"></div>
                     </div>
+                    <div id="button_p" className="ac_btn btn ">
+                      <a href="" className="text_2 text-black">Check my Resume</a>
+                      <div className="ring one"></div>
+                      <div className="ring two"></div>
+                      <div className="ring three"></div>
+                    </div>
                   </Link>
                   <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
